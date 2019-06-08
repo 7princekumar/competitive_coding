4ede2862f54d0f750a4ae1faf0ddefab31431e3c
Prince Kumar <prince.prakashsao@gmail.com>
2019-06-08 15:26:34 +0530

    solved: uva: 11727: Cost Cutting

---
+++ uva/11727.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+using namespace std;
+
+
+int main(){
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+  #endif
+
+  int tc;
+  scanf("%d", &tc);
+  int a[3];
+  while(tc--) {
+    scanf("%d %d %d", &a[0], &a[1], &a[2]);
+    sort(a, a+3);
+    cout << a[1] << endl;
+  }
+  
+  return 0;
+}
