715bbc6a5aef1b089844545e60726692ca8969fa
Prince Kumar <prince.prakashsao@gmail.com>
2019-09-27 01:01:54 +0530

    solved: aggressive_cows: codezen: tag-FR, BinarySearch

--- coding_ninjas/searching_and_sorting_applications/aggressive_cows.cpp
+++ coding_ninjas/searching_and_sorting_applications/aggressive_cows.cpp
@@ -3,7 +3,6 @@ using namespace std;
 typedef long long ll;
 
 bool is_possible(vector<ll>& stalls, ll gap, ll c) {
-    bool possible = true;
     int ccp = 0; // curr_cow_pos
     c--; //cow0 at 0th position
     for(int i=1; i<stalls.size() && c; i++) {
@@ -15,37 +14,29 @@ bool is_possible(vector<ll>& stalls, ll gap, ll c) {
     return (c == 0) ? true : false;
 }
 
-//binary search on gaps
-int bs(vector<ll>& stalls, ll start, ll end, ll c) {
-    ll ans = -1;
-    ll mid;
-    while(start <= end) {
-        mid = start + (end - start) / 2;
-        if(is_possible(stalls, mid, c)){
-            ans = mid;
-            start = mid + 1;
-        }else{
-            end = mid - 1;
-        }
-    }
-    return ans;
-}
-
 int main() {
     int tc; cin >> tc; 
     while(tc--) {
         ll n, c; cin >> n >> c; //n stall, c cows
-        vector<ll> stalls;
-        ll ele;
+        vector<ll> stalls(n);
         for(ll i=0; i<n; i++) {
-            cin >> ele;
-            stalls.push_back(ele);
+            cin >> stalls[i];
         }
         sort(stalls.begin(), stalls.end());
         ll start = 0;
         ll end = stalls[n-1] - stalls[0];
         
-        ll ans = bs(stalls, start, end, c);
+        ll ans = -1;
+        ll mid;
+        while(start <= end) {
+            mid = start + (end - start) / 2;
+            if(is_possible(stalls, mid, c)){
+                ans = mid;
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
         cout << ans << endl;
     }
     return 0;
