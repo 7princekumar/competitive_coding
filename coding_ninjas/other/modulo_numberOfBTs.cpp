e0d9bb51adbc236c976b30cdb67357ca3e294744
Prince Kumar <prince.prakashsao@gmail.com>
2020-02-28 01:17:59 +0530

    NumberOfBinaryTrees: tags-FR, Maths, ArithmeticModulo

--- coding_ninjas/other/modulo_numberOfBTs.cpp
+++ coding_ninjas/other/modulo_numberOfBTs.cpp
@@ -1,8 +1,28 @@
-// ans = x^2 + 2.x.y
-#define M (1000000007)
-int balancedBTs(int h) {
-    if(h == 0 || h == 1) return 1;
-    long long x = balancedBTs(h-1) % M;
-    long long y = balancedBTs(h-2) % M;
-    return ((x*x)%M + (2*(x*y)%M)%M) %M;
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int balancedBTs(int h){
+	if(h==0 || h==1){
+		return 1;
+	}
+	int m = pow(10,9) + 7;
+	int x = balancedBTs(h-1);
+	int y = balancedBTs(h-2);
+
+	long res1 = (long)x*x;
+	long res2 = (long)x*y*2;
+
+	int ans1 = (int)(res1%m);
+	int ans2 = (int)(res2%m);
+
+	int ans = (ans1+ans2)%m;
+
+	return ans;
+}
+
+int main(){
+	int h=8;
+	cout << balancedBTs(h) << endl;
+	return 0;
 }
